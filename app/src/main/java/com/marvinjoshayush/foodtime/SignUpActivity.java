5decebd8d71ee32b4b0489d2ad7752915e2a36b1
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2022-09-21 11:39:57 -0700

    Added button onClick function to return to log in screen

--- app/src/main/java/com/marvinjoshayush/foodtime/SignUpActivity.java
+++ app/src/main/java/com/marvinjoshayush/foodtime/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.marvinjoshayush.foodtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        setHaveAccountLogInButton();
+    }
+
+    private void setHaveAccountLogInButton() {
+        Button btn = findViewById(R.id.haveAccountLogInButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LogInActivity.class));
+            }
+        });
     }
 }
