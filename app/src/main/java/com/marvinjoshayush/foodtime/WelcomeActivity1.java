13fd8323db396c8d4d9b37b5eb2d5417e3f63e69
AyushThapaliya <ayushthapaliya1234@gmail.com>
2022-10-24 11:42:22 -0700

    Commit message firebase ###########

--- app/src/main/java/com/marvinjoshayush/foodtime/WelcomeActivity1.java
+++ app/src/main/java/com/marvinjoshayush/foodtime/WelcomeActivity1.java
@@ -1,5 +1,6 @@
 package com.marvinjoshayush.foodtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeActivity1 extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +25,10 @@ public class WelcomeActivity1 extends AppCompatActivity implements View.OnClickL
     // @+id/welcomeNext1
 
     private TextView fullname;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
 
 
@@ -30,12 +42,40 @@ public class WelcomeActivity1 extends AppCompatActivity implements View.OnClickL
         fullname = (TextView) findViewById(R.id.welcomeName);
         fullname.setOnClickListener(this);
 
-      User myname = new User();
+        User myName = new User();
 
-      fullname.setText(myname.firstname);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView welcomename = (TextView) findViewById(R.id.welcomeName);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null){
+                    String fullname = userProfile.firstname + " " + userProfile.lastname;
+
+                    welcomename.setText(fullname);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(WelcomeActivity1.this,"Error in the page!!", Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
+
 
     @Override
     public void onClick(View view) {
@@ -46,6 +86,8 @@ public class WelcomeActivity1 extends AppCompatActivity implements View.OnClickL
                 break;
 
         }
+
+
 
 
 
