9467da5305adb0144d2c842bf4c05d9987bd5b56
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2022-10-31 12:07:42 -0700

    Create basic Profile Screen layout. Make Profile Screen buttons transparent

--- app/src/main/java/com/marvinjoshayush/foodtime/ProfileFragment.java
+++ app/src/main/java/com/marvinjoshayush/foodtime/ProfileFragment.java
@@ -7,14 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
     private HomeActivity home;
+    private View view;
+    private Button[] buttons;
 
     public ProfileFragment(HomeActivity home) { this.home = home; }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        setButtons();
+
+        return view;
+    }
+
+    private void setButtons() {
+        buttons = new Button[] {view.findViewById(R.id.profileChangeName), view.findViewById(R.id.profileChangeAddress),
+                                view.findViewById(R.id.profileChangePassword), view.findViewById(R.id.profileForgotPassword)};
+
+        for(Button btn : buttons) {
+            btn.getBackground().setAlpha(0);    // Remove background (transparent background)
+        }
     }
 }
